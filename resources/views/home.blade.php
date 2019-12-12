47cd09066b145e58461ccdcec2b11f83f53a9185
Mir Monoarul Alam <14123130+mirmpro@users.noreply.github.com>
2019-12-12 10:50:45 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5"><img src="/img/29089157_178243439464821_3131302643666780160_n.jpg" class="rounded-circle" alt=""></div>
+        <div class="col-9 p-5">
+            <div><h1>anik_coder</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>148</strong> posts</div>
+                <div class="pr-4"><strong>122</strong> followers</div>
+                <div class="pr-4"><strong>148</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">A N i K</div>
+            <div>Name : Mr.Anik
+                 Hobby : Programing and Designing.
+                 Job Description : Freelance Design and Developer.
+                 Studying : Computer Engineering.
+                 Need More ?????
+            </div>
+            <div class="font-weight-bold"><a href="mirm.pro">MirM</a></div>
+        </div>
+        </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fdac13-1.fna.fbcdn.net/v/t51.2885-15/e35/74661305_2437287803186737_2395495572266619456_n.jpg?_nc_ht=instagram.fdac13-1.fna.fbcdn.net&_nc_cat=101&oh=72232842dfb9a89cdb6da1d84d9985cd&oe=5E71FDAA" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac13-1.fna.fbcdn.net/v/t51.2885-15/e35/74661305_2437287803186737_2395495572266619456_n.jpg?_nc_ht=instagram.fdac13-1.fna.fbcdn.net&_nc_cat=101&oh=72232842dfb9a89cdb6da1d84d9985cd&oe=5E71FDAA" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fdac13-1.fna.fbcdn.net/v/t51.2885-15/e35/74661305_2437287803186737_2395495572266619456_n.jpg?_nc_ht=instagram.fdac13-1.fna.fbcdn.net&_nc_cat=101&oh=72232842dfb9a89cdb6da1d84d9985cd&oe=5E71FDAA" class="w-100">
+        </div>
+    </div>
+    </div>
+</div>
+@endsection
